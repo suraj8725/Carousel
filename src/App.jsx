0ed46ab32cb05da9d8fe715288957fa2cd1d484d
Suraj Bhan Singh <surajbhan8724@gmail.com>
2024-08-23 23:27:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Carousel from "./components/Carousel";
+
+function App() {
+  return (
+    <>
+      <Carousel />
+      <h1>Created By :-</h1>
+      <h1 className="text-2xl font-bold underline">Suraj Bhan Singh</h1>
+    </>
+  );
+}
+
+export default App;
